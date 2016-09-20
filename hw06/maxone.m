e7e814b59222be0c74714e822a95844a37a40e29
PabloBrubeck <brubeck@protonmail.com>
2016-09-20 11:38:27 -0500

    Maxone

--- hw06/maxone.m
+++ hw06/maxone.m
@@ -1,18 +1,43 @@
-function [ ] = maxone( n )
-a=0;
-b=1;
-delta=1/127;
-nbits=ceil(log2((b-a)/delta+1));
+function [sup,its] = maxone(nbits, N)
+% Generate random population
+pop=randi(2^nbits,N,1)-1;
 
+% Fitness function
+f=sum(de2bi(pop),2);
+[fsup,id]=max(f);
+sup=pop(id);
 
-pob=floor(a+rand(n,1)/delta);
+its=0;
+fail=0;
+while fail<10
+    % Selection
+    p=cumsum(f/sum(f));
+    [~,j]=max(bsxfun(@ge, p, rand(1,N)));
+    pop=pop(j);
+    
+    % Reproduction
+    par1=pop(1:2:end);
+    par2=pop(2:2:end);
+    msk1=2.^randi(nbits,size(par1))-1;
+    msk1(rand(size(msk1))>0.9)=2^nbits-1;
+    msk2=(2^nbits-1)-msk1;
+    pop(1:2:end)=bitor(bitand(par1, msk1), bitand(par2, msk2));
+    pop(2:2:end)=bitor(bitand(par2, msk1), bitand(par1, msk2));
 
-f=sum(de2bi(pob),2);
-p=f/sum(f);
-
-survive=p>rand(n,1);
-
-pob(survive)
-
+    % Mutation
+    msk3=(rand(N,nbits)<0.01)*(2.^(0:nbits-1)');
+    pop=bitxor(pop, msk3);
+    
+    % Determine who has been the fittest
+    f=sum(de2bi(pop),2);
+    [fmax,id]=max(f);
+    if(fmax>=fsup)
+        fsup=fmax;
+        sup=pop(id);
+        fail=0;
+    else
+        fail=fail+1;
+    end
+    its=its+1;
 end
-
+end
